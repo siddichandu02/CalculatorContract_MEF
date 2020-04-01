33221ddd6bff1e34bd3270cef109d3c21dc25c41
Siddi <cs185304@ncr.com>
2020-04-01 14:34:26 +0530

    first

---
+++ CalculatorContract/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace CalculatorContract
+    {
+        public interface ICalculator
+        {
+            int GetNumber(int num1, int num2);
+        }
+    }
---
+++ CompositionHelper/Add.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionHelper
+{
+    [Export(typeof(ICalculator))]
+    [ExportMetadata("CalciMetaData", "Add")]
+    public class Add : ICalculator
+    {
+        #region Interface members
+        public int GetNumber(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        #endregion
+    }
+
+}
